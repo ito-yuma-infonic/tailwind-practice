f60e29e937e672f5b704e3b759415efc2ccd9c26
AzureAD\YumaIto <ito_yuma@infonic.co.jp>
2026-09-24 10:46:23 +0900

    hover

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div>
         <h1 className="text-5xl font-extrabold tracking-tight">ToDoツール</h1>
         <p>このツールの使い方です</p>
-        <button className="bg-blue-600  text-white p-3 mt-6 rounded-full shadow-2xl shadow-amber-600">
+        <button className="bg-blue-600  text-white p-3 mt-6 rounded-full shadow-2xl shadow-amber-600 hover:bg-red-700 transition duration-700">
           ツールを起動する
         </button>
         {/* 
