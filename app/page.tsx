aaeac8a99c73b99499e22a2703ecdd6790bf3e54
AzureAD\YumaIto <ito_yuma@infonic.co.jp>
2026-09-24 09:54:01 +0900

    ボタンの角、影

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,13 @@ export default function Home() {
       <div>
         <h1 className="text-5xl font-extrabold tracking-tight">ToDoツール</h1>
         <p>このツールの使い方です</p>
-        <button className="bg-blue-600  text-white p-3 mt-6">
+        <button className="bg-blue-600  text-white p-3 mt-6 rounded-full shadow-2xl shadow-amber-600">
           ツールを起動する
         </button>
+        {/* 
+        m:余白
+        p:内側の余白
+        */}
       </div>
     </>
   );
